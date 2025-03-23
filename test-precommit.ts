196d15c6206dbc9dd80ab0c4a7196164b26f93e0
Maximiliano Bove <maximiliano@rollio.ai>
2025-03-23 22:08:51 +0100

    Test pre-commit hook

---
+++ test-precommit.ts
@@ -0,0 +1,8 @@
+// This is a test file to check the pre-commit hook
+const unusedVar = 'This should trigger the linter';
+
+function testFunction(): void {
+  console.log('Hello world');
+}
+
+export default testFunction;
